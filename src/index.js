8f532cc4d132dc43f695fd97371802735f4efad6
Alexandra Weaver <aweaver0124@gmail.com>
2021-12-02 01:40:35 -0600

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+  handleTodo(e.target.new_description.value)
+})
+})
+
+function handleTodo(todo){
+  let li = document.createElement('li');
+  let button = document.createElement('button');
+  button.addEventListener('click', handleDelete)
+  button.textContent = " ♥"
+  li.textContent = `${todo} `
+  li.appendChild(button)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
